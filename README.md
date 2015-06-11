f7676c5cbcdb537570e4c1614b28f1e7c43626a0
Charlton Wilbur <cwilbur@chromatico.net>
2015-06-11 15:45:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Quiz: An Introduction to MongoDB 
+
+#### Directions
+
+* Fork and clone this quiz.
+
+* Follow the prompts in the quiz.js file, and complete each question. 
+
+* You may use any resource you like, other than another person in this classroom, to help you complete the quiz.
+
+* You can test your work by running `mongo quiz.js`
+
+* Commit and push your changes.
+
+* Submit a pull request.
+
---
+++ quiz.js
@@ -0,0 +1,72 @@
+// Quiz: An Introduction to MongoDB
+
+// do not change anything between here and question 1
+// this empties out the people collection
+
+var db = new Mongo().getDB('westeros');
+db.houses.remove({});
+
+// Question 1
+//
+// Create the following houses, with name and motto fields:
+//
+// House Arryn, motto "As High as Honor"
+// House Stark, motto "Winter is Coming"
+// House Targaryen, motto "Fire and Blood"
+
+// your code begins here
+
+
+
+
+
+// your code ends here
+
+// Question 2
+//
+// Using update and $push, add the following people to the
+// respective houses
+//
+// Ned Stark
+// Arya Stark
+// Sansa Stark
+// Viserys Targaryen
+// Daenerys Targaryen
+// Jon Arryn
+
+// your code begins here
+
+
+
+
+
+// your code ends here
+
+// Question 3
+//
+// House Arryn is not honorable!  Delete their motto.
+
+// your code begins here
+
+
+
+
+
+// your code ends here
+
+// Question 4
+//
+// You are George R. R. Martin!  Delete, without mercy,
+// House Stark.
+
+// your code begins here
+
+
+
+
+
+// your code ends here
+
+db.houses.find({}, {
+  _id: 0
+});
